4e7cebe9d43f10bdf48a2d52b204f229d5668902
Dima27011993 <73137302+Dima27011993@users.noreply.github.com>
2022-07-18 09:14:08 +0300

    wrap to map

--- src/ContentNavigation/ContentNavigationShop.jsx
+++ src/ContentNavigation/ContentNavigationShop.jsx
@@ -2,48 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import mod from "./ContentNavigationShop.module.css";
 
-let boxContentData = {
-  info:'info',
-  navlink:'add to card',
-}
+let boxContentData = [
+  {info:'info',navlink:'add to card'},
+  {info:'info',navlink:'add to card'},
+  {info:'info',navlink:'add to card'},
+  {info:'info',navlink:'add to card'},
+  {info:'info',navlink:'add to card'},
+  {info:'info',navlink:'add to card'}
+]
 
-let boxContent = ()=>{
-  return(
-    <div className={mod.boxContent}>
-    <div className={mod.contentTop}>
-      <img className={mod.photoInner} src="" alt="" />
-    </div>
-    <div className={mod.contentBottom}>
-      <p className={mod.InnerContentBox}>{boxContentData.info}</p>
-      <button className={mod.ButtonInnerContentBox}>
-        <NavLink className={mod.NavLinkContentBox} to='#'>{boxContentData.navlink}</NavLink>
-      </button>
-    </div>
+
+let NewElementBoxContent = boxContentData.map(m =><div className={mod.boxContent}>
+  <div className={mod.contentTop}>
+    <img className={mod.photoInner} src="" alt="" />
   </div>
-  )
-}
+  <div className={mod.contentBottom}>
+    <p className={mod.InnerContentBox}>{m.info}</p>
+    <button className={mod.ButtonInnerContentBox}><NavLink className={mod.NavLinkContentBox} to='#'>{m.navlink}</NavLink>
+    </button>
+  </div>
+</div>)
+
+
 
 const ContentNavigationShop = () => {
   return (
     <div>
       <div className={mod.wrapperContent}>
 
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>info</p>
-            <button className={mod.ButtonInnerContentBox}>
-              <NavLink className={mod.NavLinkContentBox} to='#'>add to card</NavLink>
-            </button>
-          </div>
-        </div>
-        {boxContent()}
-        {boxContent()}
-        {boxContent()}
-        {boxContent()}
-        {boxContent()}
+      {NewElementBoxContent}
       </div>
     </div>
   );
--- src/ContentNavigationShoppingCard/ContentNavigationShopingCard.jsx
+++ src/ContentNavigationShoppingCard/ContentNavigationShopingCard.jsx
@@ -1,105 +1,55 @@
 import React from "react";
 import mod from "./ContentNavigationShoppingCard.module.css";
+
+
+let wrapFormContentData = [
+  {label:'Name:',input:'text'},
+  {label:'Email:',input:'email'},
+  {label:'Phone:',input:'number'},
+  {label:'Adress:',input:'text'}
+]
+
+let wrapFormContentDataElement = wrapFormContentData.map(d=><div className={mod.wrapFormContent}>
+  <label>{d.label}</label>
+  <input type={d.input} placeholder="Input" />
+  </div>)
+
+  let formBoxContent = [
+    {name:'Name:',price:'Price:'},
+    {name:'Name:',price:'Price:'},
+    {name:'Name:',price:'Price:'},
+    {name:'Name:',price:'Price:'},
+    {name:'Name:',price:'Price:'},
+  ]
+  let formBoxContentElement = formBoxContent.map(d=><div className={mod.boxContent}>
+    <div className={mod.contentTop}>
+      <img className={mod.photoInner} src="" alt="" />
+    </div>
+    <div className={mod.contentBottom}>
+      <p className={mod.InnerContentBox}>{d.name}</p>
+      <p className={mod.InnerContentBox}>{d.price}</p>
+      <input type="number" />
+    </div>
+  </div>)
+  
 const ContentNavigationShoppingCard = () => {
   return (
     <div>
       <div className={mod.formShoppingCard}>
         <div className={mod.ContentLeftForm}>
           <form className={mod.formContentNavigationShoppingCard} action="">
-            <div className={mod.wrapFormContent}>
-            <label>Name:</label>
-            <input type="text" placeholder="Input" />
-            </div>
 
-            <div className={mod.wrapFormContent}>
-            <label>Email:</label>
-            <input type="email" placeholder="Input"/>
-            </div>
+            {wrapFormContentDataElement}
 
-            <div className={mod.wrapFormContent}>
-            <label>Phone:</label>
-            <input type="number" placeholder="Input" />
-            </div>
-
-            <div className={mod.wrapFormContent}>
-            <label>Adress:</label>
-            <input type="text" placeholder="Input"/>
-            </div>
           </form>
         </div>
         <div className={mod.ContentRightForm}>
 <div className={mod.ContentShopingCard}>
 
 <div className={mod.wrapperContent}>
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
-          </div>
-        </div>
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
+{formBoxContentElement}
 
-          </div>
-        </div>
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
-
-          </div>
-        </div>
-
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
-          </div>
-        </div>
-
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-            <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
-          </div>
-        </div>
-
-        <div className={mod.boxContent}>
-          <div className={mod.contentTop}>
-            <img className={mod.photoInner} src="" alt="" />
-          </div>
-          <div className={mod.contentBottom}>
-          <p className={mod.InnerContentBox}>Name:</p>
-            <p className={mod.InnerContentBox}>Price:</p>
-            <input type="number" />
-          </div>
-        </div>
       </div>
-
-
-
 
 </div>
 <div className={mod.totalPriceAndSubmit}>
@@ -109,9 +59,6 @@ const ContentNavigationShoppingCard = () => {
     <button className={mod.buttonPriceandSubmit}>Submit</button>
 </div>
 </div>
-
-
-
       </div>
     </div>
   );
